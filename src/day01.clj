5eedfb4ab5b86da7adaffbbe2738f5d5815fb1df
David Wagner <wagdav@gmail.com>
2021-11-08 15:53:16 +0100

    Setup test framework

--- src/day01.clj
+++ src/day01.clj
@@ -1,5 +1,11 @@
-(ns aoc2021.day01)
-
+(ns aoc2021.day01
+  (:require [clojure.test :as t :refer [deftest is testing run-tests]]))
 
 (defn example []
-  println ("Day 01"))
+  (println "Day 01")
+  42)
+
+(deftest test-example
+  (is (= (example) 42)))
+
+(run-tests)
