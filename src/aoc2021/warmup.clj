1c4a7074277207c60f7ffb0a435567f3ef4a641d
David Wagner <wagdav@gmail.com>
2021-11-14 22:38:34 +0100

    More warmup

--- src/aoc2021/warmup.clj
+++ src/aoc2021/warmup.clj
@@ -2,7 +2,8 @@
 ; Some tricks come from this video: https://www.youtube.com/watch?v=9ITiZ88sljA
 (ns aoc2021.warmup
   (:require [clojure.java.io :as io]
-            [clojure.test :refer [deftest testing is]]))
+            [clojure.test :refer [deftest testing is]]
+            [clojure.string :as str]))
 
 (def example [1721
               979
@@ -41,3 +42,107 @@
   (testing "Solves Part 2"
     (is (= (solve-part2 example) 241861950))
     (is (= (solve-part2 input) 292093004))))
+
+
+; Day 03 https://adventofcode.com/2020/day/3 in Clojure
+
+(def day03-example "..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#")
+
+(defn parse-tree-input [input]
+  (mapv (fn [row]
+          (mapv {\# 1 \. 0} row))
+        (str/split-lines input)))
+
+(defn tree-count
+  [right down]
+  (comp
+    (map-indexed (fn [i x] (nth (cycle x) (* i right))))
+    (drop (dec down))))
+
+;; Day 3 part 1
+(transduce (tree-count 3 1) + (parse-tree-input day03-example))
+
+;; Day 3 part 2
+(def slopes [[1 1]
+             [3 1]
+             [5 1]
+             [7 1]
+             [1 2]])
+
+(apply
+  *
+  (map
+    #(transduce (apply tree-count %) + (parse-tree-input day03-example))
+    slopes))
+
+
+(def passports "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+byr:1937 iyr:2017 cid:147 hgt:183cm
+
+iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+hcl:#cfa07d byr:1929
+
+hcl:#ae17e1 iyr:2013
+eyr:2024
+ecl:brn pid:760753108 byr:1931
+hgt:179cm
+
+hcl:#cfa07d eyr:2025 pid:166559648
+iyr:2011 ecl:brn hgt:59in")
+
+
+(defn parse-passports [input]
+  (->> (str/split input #"\n\n")
+       (map #(str/split % #"[\n ]"))
+      (map parse-passport)))
+
+(defn parse-passport [passport]
+  (->> passport
+    (mapcat #(str/split % #":"))
+    (apply hash-map)
+    clojure.walk/keywordize-keys
+    parse-values))
+
+(defn parse-year [s] (Integer/parseInt s))
+
+(defn parse-height
+  [s]
+  (let [[_ value unit] (re-find #"(\d+)(cm|in)" s)]
+    {:value (Integer/parseInt value)
+     :unit unit}))
+
+(defn parse-values
+  [passport]
+  (let [{:keys [byr iyr eyr hgt]} passport]
+    (cond-> passport
+        byr (update :byr parse-year)
+        iyr (update :iyr parse-year)
+        eyr (update :eyr parse-year)
+        hgt (update :hgt parse-height))))
+
+(defn valid? [passport]
+  (not-any? nil? ((juxt :byr :iyr :eyr :hgt :hcl :ecl :pid ) passport)))
+
+(defn valid2? [passport]
+  (and
+    (valid? passport)
+    (<= 1920 (:byr passport) 2002)
+    (<= 2010 (:iyr passport) 2020)
+    (<= 2020 (:eyr passport) 2030)))
+
+
+(valid2? {:pid "087499704" :hgt "74in" :ecl "grn" :iyr 2012 :eyr 2030 :byr 1980 :hcl "#623a2f"})
+
+(let [example (parse-passports passports)]
+  (= 2 (count (filter valid? example)))
+  (filter valid? example))
