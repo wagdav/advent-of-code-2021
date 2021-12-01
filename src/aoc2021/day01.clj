9eabbde9c0eb6a077d812039616ea501b4796e1e
David Wagner <wagdav@gmail.com>
2021-12-01 08:06:00 +0100

    Solve day 1

--- src/aoc2021/day01.clj
+++ src/aoc2021/day01.clj
@@ -1,6 +1,29 @@
 (ns aoc2021.day01
-  (:require [clojure.test :as t :refer [deftest is testing]]))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
 
-(defn example []
-  (println "Day 01")
-  42)
+(def example-input [199 200 208 210 200 207 240 269 260 263])
+
+(defn parse-input [input-file]
+  (->> (io/resource input-file)
+       slurp
+       str/split-lines
+       (mapv #(Integer/parseInt %))))
+
+(defn solve-part1 [input]
+  (->> (partition 2 1 input)
+       (filter #(apply < %))
+       count))
+
+(defn solve-part2 [input]
+  (->> (partition 3 1 input)
+       (map #(apply + %))
+       solve-part1))
+
+(= 7 (solve-part1 example-input))
+(= 5 (solve-part2 example-input))
+
+(def input (parse-input "day01.txt"))
+
+(solve-part1 input)
+(solve-part2 input)
