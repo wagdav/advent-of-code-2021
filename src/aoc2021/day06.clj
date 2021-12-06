f9aebd1f0bd054758e4c2e73a3947dd258dd85fe
David Wagner <wagdav@gmail.com>
2021-12-06 20:34:05 +0100

    Add notes on lazy sequences

--- src/aoc2021/day06.clj
+++ src/aoc2021/day06.clj
@@ -28,3 +28,38 @@
 
 (defn solve-part2 [input]
   (fish 256 input))
+
+(comment
+  (def input (frequencies (parse-input example-input)))
+
+  ; The laternfish evolution rule as a lazy sequence.
+  (defn fish-lazy-seq [st]
+    (let [new-st (-> (into {} (for [i (range 9)] [i (get st (mod (inc i) 9) 0)]))
+                     (update 6 + (get st 0 0)))]
+      (lazy-seq (cons st (fish-lazy-seq new-st)))))
+
+  ; Same as `fish-lazy-seq`, using `iterate`.  Seems clearer.
+  (defn fish-iterate [state]
+    (iterate
+      (fn [st]
+       (-> (into {} (for [i (range 9)] [i (get st (mod (inc i) 9) 0)]))
+           (update 6 + (get st 0 0))))
+      state))
+
+  ; The final aggregation function.
+  (defn fish-count [st] (reduce + (vals st)))
+
+  ; This decoupling allows to ask different questions.  For example, see the
+  ; first few days:
+  (take 8 (fish-iterate input))
+
+  ; At which day do we overcome 10k lanternfish?
+  (->> (fish-iterate input)
+       (map-indexed (fn [idx st] [idx (fish-count st)]))
+       (drop-while (fn [[_ cnt]] (<= cnt 10000)))
+       ffirst)
+
+  ; Solution of the puzzle: "How many lanterfish would there be after 256
+  ; days?"
+  (-> (nth (fish-iterate input) 256)
+      fish-count))
