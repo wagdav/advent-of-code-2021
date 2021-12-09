6566c3ab924381b77a746820c70af583a3eda0c4
David Wagner <wagdav@gmail.com>
2021-12-09 07:25:30 +0100

    Solve Day 8 with core.logic

---
+++ src/aoc2021/day08b.clj
@@ -0,0 +1,38 @@
+(ns aoc2021.day08b
+  (:refer-clojure :exclude [==])
+  (:use clojure.core.logic)
+  (:require [clojure.set :as set]
+            [aoc2021.day08 :refer [decode]]))
+
+(defn overlay [a b c]
+  (project [a b c]
+    (== true
+        (= (set/union (set a) (set b)) (set c)))))
+
+(defn decode-logic [candidates]
+  (let [length (group-by count candidates)]
+    (first
+      (run 1 [q]
+        (fresh [zero one two three four five six seven eight nine]
+          (== q {zero 0 one 1 two 2 three 3 four 4 five 5 six 6 seven 7 eight 8 nine 9})
+
+          (== one (first (length 2)))
+          (== four (first (length 4)))
+          (== seven (first (length 3)))
+          (== eight (first (length 7)))
+
+          (permuteo (length 5) [two three five])
+          (permuteo (length 6) [zero six nine])
+
+          (overlay one three three)
+          (overlay one nine nine)
+          (overlay one zero zero)
+          (overlay one five nine)
+
+          (permuteo candidates [zero one two three four five six seven eight nine]))))))
+
+(comment
+  (def candidates ["be" "cfbegad" "cbdgef" "fgaecd" "cgeb" "fdcge" "agebfd" "fecdb" "fabcd" "edb"])
+  (time (decode candidates)) ; takes less than 1 msec
+  (time (decode-logic candidates)) ; takes ~6 seconds
+  (= (decode-logic candidates) (decode candidates)))
