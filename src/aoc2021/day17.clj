1cab1e1c46cf2515f543e05307a8472f20c99c1a
David Wagner <wagdav@gmail.com>
2021-12-19 14:25:50 +0100

    Finish Day 17.
    To finally figure out the correct vy ranges I took inspiration from
    here: https://github.com/lizthegrey/adventofcode/blob/main/2021/day17.go

--- src/aoc2021/day17.clj
+++ src/aoc2021/day17.clj
@@ -27,94 +27,32 @@
        :vy (dec vy))
     (max-height max-y)))
 
-(defn probe [vx vy]
-  (iterate step {:t 0 :x 0 :y 0 :vx vx :vy vy}))
-
-(step {:vx 6 :vy 7})
-
-(take 8 (probe 7 2))
-
-(take-while (fn [state] (<= (state :t) 10)) (probe 6 9))
-
 (defn in-target? [{:keys [x y]} [left right bot top]]
-  (and (<= left x right)
-       (<= bot y top)))
+  (and (<= left x right) (<= bot y top)))
 
-(in-target? {:x 21 :y -6} [20 30 -10 -5])
+(defn can-reach-target? [{:keys [x y]} [_ right bot _]]
+  (and (<= x right) (<= bot y)))
 
-(defn until-x [x-limit vx]
-  (loop [state (step {:vx vx :vy 0})]
-    (if (or (= 0 (state :vx)) (<= x-limit (state :x)))
-      state
-      (recur (step state)))))
-
-(until-x 20 9)
-
-(defn scan-vx [[left right _ _]]
-  (loop [vx     1
-         result []]
-
-    (let [final (until-x right vx)]
-      (println "vx=" vx "final=" final)
-      (cond
-        (< (final :x) left)
-        (recur (+ vx 1) result)
-
-        (<= left (final :x) right)
-        (recur (+ vx 1) (conj result vx))
-
-        :else
-        result))))
-
-(scan-vx target)
-
-(defn until-t [vx vy [left right bottom top]]
+(defn shoot [vx vy target]
   (loop [state (step {:t 0 :x 0 :y 0 :vx vx :vy vy})]
-    (cond
-      (in-target? state [left right bottom top])
-      (assoc state :end :on-target)
+    (if (can-reach-target? state target)
+      (if (in-target? state target)
+        state
+        (recur (step state)))
+      state)))
 
-      (<= (state :y) bottom)
-      (assoc state :end :bottom)
-
-      (<= right (state :x))
-      (assoc state :end :right)
-
-      :else
-      (recur (step state)))))
-
-(defn scan-vy [vxs [left right bot top]]
-  (for
-    [vx vxs]
-    (loop [vy -100
-           heights []
-           dir :under]
-
-      (let [final (until-t vx vy [left right bot top])]
-        (do
-          (println "vx=" vx "vy=" vy "final=" final)
-          (cond
-            (= (:end final) :on-target)
-            (recur (inc vy) (conj heights (final :max-y)) :over)
-
-            (and (= (:end final) :bottom)
-                 (= dir :under))
-            (recur (inc vy) heights :under)
-
-            :else
-            heights))))))
+(defn scan-vy [[left right bot top]]
+  (filter
+    #(in-target? % [left right bot top])
+    (for [vx (range 0 (inc right))
+          vy (range bot (- 1 bot))]
+      (shoot vx vy [left right bot top]))))
 
 (defn solve-part1 [target]
-  (apply max (flatten (scan-vy (scan-vx target) target))))
+  (->> (scan-vy target)
+       (map :max-y)
+       (apply max)))
 
-(defn solve-part2 [input])
-
-(comment
-  (def target [20 30 -10 -5])
-  (scan-vy (scan-vx target) target)
-
-  (def target [244 303 -91 -54])
-  (scan-vy (scan-vx target) target)
-
-  (solve-part1 target))
-
+(defn solve-part2 [target]
+  (->> (scan-vy target)
+       (count)))
--- test/aoc2021/day17_test.clj
+++ test/aoc2021/day17_test.clj
@@ -6,13 +6,14 @@
                                    solve-part1
                                    solve-part2]]))
 
-(testing "with example input input"
-  (let [input (parse-input example-input)]
-    (is (= 45 (solve-part1 input)))
-    #_(is (= 0 (solve-part2 input)))))
+(deftest working
+  (testing "with example input input"
+    (let [input (parse-input example-input)]
+      (is (= 45 (solve-part1 input)))
+      (is (= 112 (solve-part2 input)))))
 
-(testing "with real input"
-  (let [input (parse-input (slurp (io/resource "day17.txt")))]
-    (is (= 990 (solve-part1 input))) ; wrong
-    #_(is (= 0 (solve-part2 input)))))
+  (testing "with real input"
+    (let [input (parse-input (slurp (io/resource "day17.txt")))]
+      (is (= 4095 (solve-part1 input)))
+      (is (= 3773 (solve-part2 input))))))
 
